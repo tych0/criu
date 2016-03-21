7f0cb1b8af4a7aa7bf199bc22c1884e2266c58c9
Tycho Andersen <tycho.andersen@canonical.com>
2016-03-21 10:35:35 -0600

    cgroup: compare mount root too
    For cgroup namespaces, we need to compare the root of the mounts to see if
    the superblocks match. The sub-path that the namespace is in is listed as
    the mount root, e.g.,
    
    139 136 0:28 /lxc/unpriv /sys/fs/cgroup/freezer rw,nosuid,nodev,noexec,relatime - cgroup cgroup rw,freezer,clone_children
    
    is a mount of the freezer cgroup under /lxc/unpriv.
    
    Signed-off-by: Tycho Andersen <tycho.andersen@canonical.com>

--- criu/mount.c
+++ criu/mount.c
@@ -319,6 +319,9 @@ static bool mounts_sb_equal(struct mount_info *a, struct mount_info *b)
 			return false;
 	}
 
+	if (a->fstype->code == FSTYPE__CGROUP && strcmp(a->root, b->root))
+		return false;
+
 	return a->s_dev == b->s_dev && !strcmp(a->source, b->source);
 }
 
