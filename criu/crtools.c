0d41d4be84b5a853f01e3fb95570c771d2d7c21d
Mike Rapoport <rppt@linux.vnet.ibm.com>
2016-04-11 09:19:45 +0300

    lazy-pages: always compile uffd.c
    If CONFIG_HAS_UFFD is not defined an attempt to run the lazy pages daemon
    will result in error message
    
    Signed-off-by: Mike Rapoport <rppt@linux.vnet.ibm.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/crtools.c
+++ criu/crtools.c
@@ -283,9 +283,7 @@ int main(int argc, char *argv[], char *envp[])
 		{ "timeout",			required_argument,	0, 1072 },
 		{ "external",			required_argument,	0, 1073	},
 		{ "empty-ns",			required_argument,	0, 1074	},
-#ifdef CONFIG_HAS_UFFD
 		{ "lazy-pages",			no_argument,		0, 1076 },
-#endif
 		{ "extra",			no_argument,		0, 1077	},
 		{ "experimental",		no_argument,		0, 1078	},
 		{ "all",			no_argument,		0, 1079	},
@@ -548,11 +546,9 @@ int main(int argc, char *argv[], char *envp[])
 		case 1072:
 			opts.timeout = atoi(optarg);
 			break;
-#ifdef CONFIG_HAS_UFFD
 		case 1076:
 			opts.lazy_pages = true;
 			break;
-#endif
 		case 'M':
 			{
 				char *aux;
@@ -799,9 +795,7 @@ usage:
 "  criu page-server\n"
 "  criu service [<options>]\n"
 "  criu dedup\n"
-#ifdef CONFIG_HAS_UFFD
 "  criu lazy-pages -D DIR [<options>]\n"
-#endif
 "\n"
 "Commands:\n"
 "  dump           checkpoint a process/tree identified by pid\n"
@@ -840,12 +834,10 @@ usage:
 "     --exec-cmd         execute the command specified after '--' on successful\n"
 "                        restore making it the parent of the restored process\n"
 "  --freeze-cgroup       use cgroup freezer to collect processes\n"
-#ifdef CONFIG_HAS_UFFD
 "  --lazy-pages          restore pages on demand\n"
 "                        this requires running a second instance of criu\n"
 "                        in lazy-pages mode: 'criu lazy-pages -D DIR'\n"
 "                        --lazy-pages and lazy-pages mode require userfaultfd\n"
-#endif
 "\n"
 "* Special resources support:\n"
 "  -x|--" USK_EXT_PARAM " [inode,...]\n"
