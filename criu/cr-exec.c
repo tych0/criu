e36af21b70e428aed268dae431dd6a830b3c75ab
Dmitry Safonov <dsafonov@virtuozzo.com>
2016-04-12 19:10:42 +0300

    cr-exec: add non-generated sys-exec-tbl for x86
    Impact: search sys-exec-tbl-32.c for compatible tasks.
    
    Rename task_in_compat_mode to arch_task_compatible and use it in
    find_syscall for 64-bit to check compatible task's syscall nr.
    It still will not execute syscall in 32-tasks, as we still do not
    have 32-bit pie (arch_can_dump_task will return false for these tasks).
    
    NOTE: be sure to `make mrproper` on criu directory before applying this
    patch, as before `criu/arch/x86/sys-exec-tbl.c` was autogenerated,
    it will make conflict if you try to apply this patch on dirty directory.
    
    Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>
    Acked-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/cr-exec.c
+++ criu/cr-exec.c
@@ -19,7 +19,9 @@ struct syscall_exec_desc {
 #include "sys-exec-tbl.c"
 #undef SYSCALL
 
-static struct syscall_exec_desc *find_syscall(char *name)
+#ifndef ARCH_HAS_FIND_SYSCALL
+struct syscall_exec_desc *
+find_syscall(char *name, int __attribute__((unused)) pid)
 {
 	int i;
 
@@ -29,6 +31,7 @@ static struct syscall_exec_desc *find_syscall(char *name)
 
 	return NULL;
 }
+#endif
 
 #define MAX_ARGS	6
 
@@ -130,7 +133,7 @@ int cr_exec(int pid, char **opt)
 		goto out;
 	}
 
-	si = find_syscall(sys_name);
+	si = find_syscall(sys_name, pid);
 	if (!si) {
 		pr_err("Unknown syscall [%s]\n", sys_name);
 		goto out;
