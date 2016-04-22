66afbe12b3594b024938d8aa2cafa4798cde809b
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-04-22 13:52:00 +0300

    sources: fix pr_perror usage
    This is log cleanup patch.
    Pr_perror prints new line by itself.
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/mount.c
+++ criu/mount.c
@@ -3422,7 +3422,7 @@ int prepare_mnt_ns(void)
 			if (mount("proc", "/proc", "proc",
 						MS_MGC_VAL | MS_NOSUID | MS_NOEXEC | MS_NODEV,
 						NULL)) {
-				pr_perror("Can't mount proc\n");
+				pr_perror("Can't mount proc");
 				return -1;
 			}
 
