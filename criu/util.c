tych0/criu

commit 1dc2f87b33ce4263a031512d1e04421de5db979e

util: shutdown log in cr_system_userns if error fd is negative
Otherwise error in case of exec error won't be printed
The problem is that when err fd is negative, it's replaced by log fd.
Then err is moved to STDERR (that means, that log_fd is _closed_).
But log facility still consider log fd as valid and tries to use it to print
error message in case of exec failure.
Which is equal to writing to /dev/null, basically.
This patch shutdown log, if err fd was negative, thus forcing criu to output
exec error to STDERR (which was replaced by log fs, btw).

Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

criu/util.c
@@ -609,6 +609,8 @@ int cr_system_userns(int in, int out, int err, char *cmd,
 		pr_perror("fork() failed");
 		goto out;
 	} else if (pid == 0) {
+		bool stop_log_fd = false;
+
 		if (userns_pid > 0) {
 			if (switch_ns(userns_pid, &user_ns_desc, NULL))
 				goto out_chld;
@@ -620,8 +622,10 @@ int cr_system_userns(int in, int out, int err, char *cmd,
 
 		if (out < 0)
 			out = log_get_fd();
-		if (err < 0)
+		if (err < 0) {
 			err = log_get_fd();
+			stop_log_fd = true;
+		}
 
 		/*
 		 * out, err, in should be a separate fds,
@@ -636,6 +640,9 @@ int cr_system_userns(int in, int out, int err, char *cmd,
 		if (move_img_fd(&out, STDIN_FILENO) ||
 		    move_img_fd(&err, STDIN_FILENO))
 			goto out_chld;
+
+		if (stop_log_fd)
+			log_fini();
 
 		if (in < 0) {
 			close(STDIN_FILENO);

commit 16ecb46205f010e76bc8c520d48b20445a06cf74

util: cr_system_userns() should not affect log_fd
Fixes: 1dc2f87b33ce ("util: shutdown log in cr_system_userns if error fd is negative")
Cc: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
Signed-off-by: Andrew Vagin <avagin@virtuozzo.com>
Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

criu/util.c
@@ -609,8 +609,6 @@ int cr_system_userns(int in, int out, int err, char *cmd,
 		pr_perror("fork() failed");
 		goto out;
 	} else if (pid == 0) {
-		bool stop_log_fd = false;
-
 		if (userns_pid > 0) {
 			if (switch_ns(userns_pid, &user_ns_desc, NULL))
 				goto out_chld;
@@ -621,11 +619,9 @@ int cr_system_userns(int in, int out, int err, char *cmd,
 		}
 
 		if (out < 0)
-			out = log_get_fd();
-		if (err < 0) {
-			err = log_get_fd();
-			stop_log_fd = true;
-		}
+			out = DUP_SAFE(log_get_fd(), out_chld);
+		if (err < 0)
+			err = DUP_SAFE(log_get_fd(), out_chld);
 
 		/*
 		 * out, err, in should be a separate fds,
@@ -640,9 +636,6 @@ int cr_system_userns(int in, int out, int err, char *cmd,
 		if (move_img_fd(&out, STDIN_FILENO) ||
 		    move_img_fd(&err, STDIN_FILENO))
 			goto out_chld;
-
-		if (stop_log_fd)
-			log_fini();
 
 		if (in < 0) {
 			close(STDIN_FILENO);