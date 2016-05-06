4fc6381c9789f7bf9b432685111199b8a458df16
Tycho Andersen <tycho.andersen@canonical.com>
2016-05-06 12:51:33 -0500

    opts: add a --leave-frozen option
    In LXD, we want to invoke criu, but leave tasks in the freezer until we are
    sure that the restore on the other side worked, and then kill the tasks.
    
    v2: don't unlock the network when in --leave-frozen mode
    
    Signed-off-by: Tycho Andersen <tycho.andersen@canonical.com>

--- criu/cr-dump.c
+++ criu/cr-dump.c
@@ -1491,7 +1491,7 @@ int cr_pre_dump_tasks(pid_t pid)
 		opts.track_mem = true;
 	}
 
-	if (opts.final_state == TASK_DEAD) {
+	if (opts.final_state == TASK_DEAD || opts.final_state == TASK_FROZEN) {
 		pr_info("Enforcing tasks run after pre-dump.\n");
 		opts.final_state = TASK_ALIVE;
 	}
