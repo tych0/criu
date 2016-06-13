a60b4126577b00861960f01562faf720ff283759
Tycho Andersen <tycho.andersen@canonical.com>
2016-06-13 23:24:48 +0000

    net: add support for macvlan link types
    While this is in principle similar to how veths are handled, we have to do
    things in two different ways depending on whether or not there is a user
    namespace involved, because there is no way to ask the kernel to attach a
    macvlan NIC to a device in a net ns that we don't have CAP_NET_ADMIN in.
    
    So we do it in two ways:
    
    a. If we are in a user namespace, we create the device in usernsd and use
       IFLA_NET_NS_PID to set the netns which it should be created in (saving
       us a "move into this netns" step).
    
    b. If we aren't in a user namespace, we could still be in a net namespace,
       so we use IFLA_LINK_NETNSID to set namespace that the i/o device will be
       in. Then we open a netlink socket from criu's netns and use
       IFLA_NET_NS_FD to tell the kernel to create the macvlan device in the
       target's namespace.
    
    v2: * s/CLONE_NEWNET/CLONE_NEWUSER
        * Don't bother to dump IFLA_LINK and IFLA_LINK_NETNSID. Although we
          need to provide these on restore, there's no kernel interface that
          persists these. To populate IFLA_LINK, we require users pass
          --macvlan-pair, and we create a NETNSID relation as needed and pass
          that in for macvlan links (although this infrastructure could be used
          elsewhere for links that need it in the future, since is in the
          hoisted populate_newlink_req()).
        * use new external command instead of creating a --macvlan-pair option
    
    v3: add a feature check for linux/net_namespace.h, since not every arch in
        travis has this (new-ish) header
    
    Signed-off-by: Tycho Andersen <tycho.andersen@canonical.com>

--- criu/include/net.h
+++ criu/include/net.h
@@ -4,6 +4,7 @@
 #include <linux/netlink.h>
 
 #include "list.h"
+#include "external.h"
 
 struct cr_imgset;
 extern int dump_net_ns(int ns_id);
@@ -15,6 +16,12 @@ struct veth_pair {
 	char *inside;
 	char *outside;
 	char *bridge;
+};
+
+struct macvlan_pair {
+	struct list_head node;
+	char *inside;
+	int ifi_outside;
 };
 
 extern int collect_net_namespaces(bool for_dump);
@@ -30,6 +37,7 @@ extern int read_ns_sys_file(char *path, char *buf, int len);
 extern int restore_link_parms(NetDeviceEntry *nde, int nlsk);
 
 extern int veth_pair_add(char *in, char *out);
+extern int macvlan_ext_add(struct external *ext);
 extern int move_veth_to_bridge(void);
 
 #endif /* __CR_NET_H__ */
