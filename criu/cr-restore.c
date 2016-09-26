25ca00580b6a190e2297599baa2dcf8513c89c83
Pavel Emelyanov <xemul@virtuozzo.com>
2016-09-26 13:20:00 +0300

    parasite: Pass exec start point to parasite_prep_ctl
    The parasite_prep_ctl() will become compel call, so it won't
    have the code that scans CRIU's vma area list. For pure compel
    users we'll need to add simple /proc/maps scanner that'd find
    the entry point.
    
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/cr-restore.c
+++ criu/cr-restore.c
@@ -1647,7 +1647,7 @@ static void finalize_restore(void)
 			continue;
 
 		/* Unmap the restorer blob */
-		ctl = parasite_prep_ctl(pid, NULL);
+		ctl = parasite_prep_ctl(pid, 0);
 		if (ctl == NULL)
 			continue;
 
