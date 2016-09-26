57390b483168ccfa6e2866de3f07b58f75fdf53e
Cyrill Gorcunov <gorcunov@openvz.org>
2016-09-26 22:36:36 +0300

    compel: plugins,std -- Add syscall-types.h header
    Both std and criu will use it for syscalls sake.
    Note I've to disable x86 compat mode for a while:
    we have to provide native types there thus will
    back once everything else is complete.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/arch/aarch64/include/asm/types.h
+++ criu/arch/aarch64/include/asm/types.h
@@ -10,33 +10,10 @@
 #include "asm/bitops.h"
 #include "asm/int.h"
 
+#include "uapi/std/asm/syscall-types.h"
 
 #define SIGMAX			64
 #define SIGMAX_OLD		31
-
-typedef void rt_signalfn_t(int, siginfo_t *, void *);
-typedef rt_signalfn_t *rt_sighandler_t;
-
-typedef void rt_restorefn_t(void);
-typedef rt_restorefn_t *rt_sigrestore_t;
-
-#define _KNSIG		64
-#define _NSIG_BPW	64
-
-#define _KNSIG_WORDS	(_KNSIG / _NSIG_BPW)
-
-typedef struct {
-	unsigned long sig[_KNSIG_WORDS];
-} k_rtsigset_t;
-
-#define SA_RESTORER	0x00000000
-
-typedef struct {
-	rt_sighandler_t	rt_sa_handler;
-	unsigned long	rt_sa_flags;
-	rt_sigrestore_t	rt_sa_restorer;
-	k_rtsigset_t	rt_sa_mask;
-} rt_sigaction_t;
 
 /*
  * Copied from the Linux kernel header arch/arm64/include/uapi/asm/ptrace.h
