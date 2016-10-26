28ec6e1f1dcd46449dfcd16238e41c4964e17047
Tycho Andersen <tycho.andersen@canonical.com>
2016-10-26 14:56:07 +0000

    util: add rm -rf function
    Signed-off-by: Tycho Andersen <tycho.andersen@canonical.com>

--- criu/include/util.h
+++ criu/include/util.h
@@ -278,6 +278,9 @@ int setup_tcp_server(char *type);
 int run_tcp_server(bool daemon_mode, int *ask, int cfd, int sk);
 int setup_tcp_client(char *addr);
 
+/* *dir should be writable and at least PATH_MAX long */
+int rm_rf(char *dir);
+
 #define LAST_PID_PATH		"sys/kernel/ns_last_pid"
 #define PID_MAX_PATH		"sys/kernel/pid_max"
 
