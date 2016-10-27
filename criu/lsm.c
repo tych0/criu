7a92a99a5af2339c0147382d7603569910800db6
Tycho Andersen <tycho.andersen@canonical.com>
2016-10-27 22:09:38 +0000

    apparmor: add support for suspending the LSM from userspace
    The idea here is that we need something like PTRACE_O_SUSPEND_SECCOMP for
    apparmor. The strategy here is just to replace the apparmor profile with
    one that will work for all of the stuff CRIU needs to do, and then write
    the old profile back at the end of the dump if necessary.
    
    The patch in the next test uses a profile without "unix", to test this.
    
    v2: use rm_rf instead of rmdir to clean up the policy directory, since
        apparmor_parser leaves all sorts of cache files and such in it.
    v3: support profiles that include a '/'

--- criu/lsm.c
+++ criu/lsm.c
@@ -193,6 +193,10 @@ int collect_and_suspend_lsm(void)
 	/* now, suspend the LSM; this is where code that implements something
 	 * like PTRACE_O_SUSPEND_LSM should live. */
 	switch(lsmtype) {
+	case LSMTYPE__APPARMOR:
+		if (suspend_aa() < 0)
+			return -1;
+		break;
 	default:
 		pr_warn("don't know how to suspend LSM %d\n", lsmtype);
 	}
@@ -202,6 +206,9 @@ int collect_and_suspend_lsm(void)
 
 int unsuspend_lsm(void)
 {
+	if (lsmtype == LSMTYPE__APPARMOR && unsuspend_aa())
+		return -1;
+
 	return 0;
 }
 
