ffc44a93dabba096bbb23537b87a1ddec2608fc1
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-06-07 14:29:09 +0300

    files: Teach collect_fd() mark fake files
    Add new argument to this function.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/files.c
+++ criu/files.c
@@ -772,7 +772,7 @@ compare_pid:
 	list_add_tail(&new_le->desc_list, &le->desc_list);
 }
 
-int collect_fd(int pid, FdinfoEntry *e, struct rst_info *rst_info)
+int collect_fd(int pid, FdinfoEntry *e, struct rst_info *rst_info, bool fake)
 {
 	struct fdinfo_list_entry *new_le;
 	struct file_desc *fdesc;
@@ -783,6 +783,7 @@ int collect_fd(int pid, FdinfoEntry *e, struct rst_info *rst_info)
 	new_le = alloc_fle(pid, e);
 	if (!new_le)
 		return -1;
+	new_le->fake = (!!fake);
 
 	fdesc = find_file_desc(e);
 	if (fdesc == NULL) {
@@ -821,7 +822,7 @@ int dup_fle(struct pstree_item *task, struct fdinfo_list_entry *ple,
 	if (!e)
 		return -1;
 
-	return collect_fd(vpid(task), e, rsti(task));
+	return collect_fd(vpid(task), e, rsti(task), false);
 }
 
 int prepare_ctl_tty(int pid, struct rst_info *rst_info, u32 ctl_tty_id)
@@ -843,7 +844,7 @@ int prepare_ctl_tty(int pid, struct rst_info *rst_info, u32 ctl_tty_id)
 	e->fd		= reserve_service_fd(CTL_TTY_OFF);
 	e->type		= FD_TYPES__TTY;
 
-	if (collect_fd(pid, e, rst_info)) {
+	if (collect_fd(pid, e, rst_info, false)) {
 		xfree(e);
 		return -1;
 	}
@@ -883,7 +884,7 @@ int prepare_fd_pid(struct pstree_item *item)
 			break;
 		}
 
-		ret = collect_fd(pid, e, rst_info);
+		ret = collect_fd(pid, e, rst_info, false);
 		if (ret < 0) {
 			fdinfo_entry__free_unpacked(e, NULL);
 			break;
