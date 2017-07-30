a6f77385eef64fcdff2057de73754c7ab199d2a9
Mike Rapoport <rppt@linux.vnet.ibm.com>
2017-07-30 09:29:20 +0300

    util/epoll: do not report polling timeout
    It's not really interesting and just pollutes the log
    
    Signed-off-by: Mike Rapoport <rppt@linux.vnet.ibm.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/util.c
+++ criu/util.c
@@ -1310,8 +1310,6 @@ int epoll_run_rfds(int epollfd, struct epoll_event *evs, int nr_fds, int timeout
 		if (ret <= 0) {
 			if (ret < 0)
 				pr_perror("polling failed");
-			else
-				pr_debug("polling timeout\n");
 			break;
 		}
 
