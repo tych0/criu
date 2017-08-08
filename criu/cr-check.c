f042d65d3ff8a722393e1567e9b0dfdbbac6d307
Mike Rapoport <rppt@linux.vnet.ibm.com>
2017-08-08 14:44:29 +0300

    cr-check: make uffd verification "extra" rather then "experimental"
    Signed-off-by: Mike Rapoport <rppt@linux.vnet.ibm.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/cr-check.c
+++ criu/cr-check.c
@@ -1226,6 +1226,8 @@ int cr_check(void)
 		ret |= check_ns_get_parent();
 		ret |= check_pid_for_children_ns();
 		ret |= check_can_map_vdso();
+		ret |= check_uffd();
+		ret |= check_uffd_noncoop();
 	}
 
 	/*
