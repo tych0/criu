699b9b0d5cc07fdb301c7a6f4ef73766757e679b
Alice Frosi <alice@linux.vnet.ibm.com>
2017-10-05 15:50:39 +0200

    Restore registers not present in the sigreturn signal frame
    Add new function arch_set_task_regs_nosigrt(). It allows to restore
    architecture-specific registers not present in sigreturn signal frame.
    
    Each architecture can overwrite this function.
    
    The arch_set_task_regs_nosigrt function restores the registers between the
    final sigreturn and PTRACE_DETACH. We do this at this point because
    we are sure that all threads have already been created.
    
    Add arch_set_thread_regs in pre-dump because some registers may have
    been changed by the parasite infection and they are not present in
    sigreturn signal frame.
    
    Signed-off-by: Alice Frosi <alice@linux.vnet.ibm.com>
    Reviewed-by: Michael Holzheu <holzheu@linux.vnet.ibm.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/include/restore.h
+++ criu/include/restore.h
@@ -1,8 +1,11 @@
 #ifndef __CR_INC_RESTORE_H__
 #define __CR_INC_RESTORE_H__
+
+#include "pid.h"
 #include "types.h"
 #include "asm/restore.h"
 
 extern int criu_signals_setup(void (*handler)(int, siginfo_t *, void *));
+extern int arch_set_thread_regs_nosigrt(struct pid *pid);
 
 #endif
